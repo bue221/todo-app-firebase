11ac2f3e9a56975e89415ec1686312a3f4160d8d
bue221 <camiloplaza3@gmail.com>
2020-07-23 16:49:43 -0500

    Updates

---
+++ precache-manifest.4adbafe015c14ca58c44f40aad64deb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbea49f654922beecd2f885c65255d4b",
+    "url": "/todo-app-firebase/index.html"
+  },
+  {
+    "revision": "e8fa8a6f439be470344f",
+    "url": "/todo-app-firebase/static/css/main.f56d6f5b.chunk.css"
+  },
+  {
+    "revision": "4dd7f816e945423733a8",
+    "url": "/todo-app-firebase/static/js/2.d36b3cda.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/todo-app-firebase/static/js/2.d36b3cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8fa8a6f439be470344f",
+    "url": "/todo-app-firebase/static/js/main.abf483c5.chunk.js"
+  },
+  {
+    "revision": "a275b78302dc7016de47",
+    "url": "/todo-app-firebase/static/js/runtime-main.a3693cab.js"
+  }
+]);
